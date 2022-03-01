dfe7221c0752006b1bc21145ce77e3cb1a3e2238
tylusdawkins <tylusjdawkins@gmail.com>
2022-03-01 06:12:42 -0800

    first commit?

--- u1_hw_tic_tac_toe/script.js
+++ u1_hw_tic_tac_toe/script.js
@@ -9,47 +9,6 @@ let oArray = []
 winner=false
 isGameActive=true
 
-//have to switch between X and O
-//need variable called turns starting off at 0
-//using some math operator use js logic to decide wether or not our turn is even or odd with that we can decide we have player X or O
-//if turns modulo 2 =0 then it is player x if turns modulo 2 =1 then is it O, use math.ceiling
-
-//check if the number is even
-// if(number % 2 == 0) {
-//     console.log("The number is even.");
-// }
-
-// // if the number is odd
-// else {
-//     console.log("The number is odd.");
-// }
-
-// have board2 with 9 positions, list player as 1 or 2 in each position in the index based on their chosen position
-
-// have each box effect position of array based on index b1 effects board[1]
-
-
-// let userAction = ()=>{
-//     if(turns % 2 == 0){
-//         boxes[i].innerText=playerX
-//         turns=+1
-//     }
-//     else if(turns % 2 == 1){
-//         boxes[i].innerText=playerO
-//         turns=+1
-//     }
-// }
-
-
-
-// if(turns % 2 == 0){
-//     board2[box].push="x"
-// }
-// else{
-//     board2[box].push="o"
-//     }
-// }
-
 const findBox = (box) =>{
     if(turns % 2 == 0){
         board[box] ="x"
