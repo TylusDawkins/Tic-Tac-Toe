f869282060586f743da533f86a72bde44fd2296d
tylusdawkins <tylusjdawkins@gmail.com>
2022-02-28 18:15:14 -0800

    third iterration

--- Tic_Tac_Toe_Tylus/script.js
+++ Tic_Tac_Toe_Tylus/script.js
@@ -1,12 +1,13 @@
 const boxes = document.querySelectorAll('.box')
 const resetButton = document.querySelector('#reset')
-let board = ['','','','','','','','']
-let board2 = [0,0,0,0,0,0,0,0,0]
+let board = [0,0,0,0,0,0,0,0,0]
 let turns = 0
 const playerX = "X"
 let xArray = []
 const playerO = "O"
 let oArray = []
+winner=false
+isGameActive
 
 //have to switch between X and O
 //need variable called turns starting off at 0
@@ -39,6 +40,50 @@ let oArray = []
 //     }
 // }
 
+
+
+// if(turns % 2 == 0){
+//     board2[box].push="x"
+// }
+// else{
+//     board2[box].push="o"
+//     }
+// }
+
+const findBox = (box) =>{
+    if(turns % 2 == 0){
+        board[box] ="x"
+        console.log(board)
+    }
+    else{
+        board[box] ="o"
+        console.log(board)
+    }
+}
+
+for(let i = 0; i<boxes.length; i++){
+    boxes[i].addEventListener('click', function(){
+        if(isGameActive = true){
+            if(turns % 2 == 0){
+                boxes[i].innerText=playerX
+                turns++;
+                handleResultValidation
+                // checkWinner
+                console.log(winner)
+            }
+        
+            else if(turns % 2 == 1){
+                boxes[i].innerText=playerO
+                turns++;
+                handleResultValidation
+                // checkWinner
+                console.log(winner)
+        
+        }
+    }
+    })
+}
+
 const winningConditions = [
     // horizontal win conditions
     [0, 1, 2],
@@ -53,34 +98,56 @@ const winningConditions = [
     [2, 4, 6]
 ];
 
-// if(turns % 2 == 0){
-//     board2[box].push="x"
-// }
-// else{
-//     board2[box].push="o"
+
+// const checkWinner = () => {
+
+
+//     const boxValue1 = board[winningConditions[0]]
+//     const boxValue2 = board[winningConditions[1]]
+//     const boxValue3 = board[winningConditions[2]]
+
+//     if (boxValue1 != null && boxValue1 === boxValue2 && boxValue2 === boxValue3) {
+//         gameOverScreen(boxValue1)
+//         winner = true
+//         } 
+//         else {
+//         const drawGame = board.every((box)=> box != null)
+//         if (drawGame) {
+//         gameOverScreen(null)
+//         }
 //     }
 // }
 
-const findBox = (box) =>{
-    if(turns % 2 == 0){
-        board2[box] ="x"
-        console.log(board2)
-    }
-    else{
-        board2[box] ="o"
-        console.log(board2)
-    }
-}
 
-for(let i = 0; i<boxes.length; i++){
-    boxes[i].addEventListener('click', function(){
-        if(turns % 2 == 0){
-            boxes[i].innerText=playerX
-            turns++;
-        }
-        else if(turns % 2 == 1){
-            boxes[i].innerText=playerO
-            turns++;
-        }
-    })
+// function handleResultValidation() {
+//     let roundWon = false;
+//     for (let i = 0; i <= 7; i++) {
+//         const winCondition = winningConditions[i];
+//         const a = board[winCondition[0]];
+//         const b = board[winCondition[1]];
+//         const c = board[winCondition[2]];
+//         if (a === '' || b === '' || c === '') {
+//             continue;
+//         }
+//         if (a == b && b == c) {
+//             roundWon = true;
+//             winner = true;
+//             break;
+//         }
+//     }
+
+//     if (roundWon) {
+//         announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+//         isGameActive = false;
+//         return;
+//     }
+
+//     if (!board.includes(''))
+//     announce(TIE);
+// }
+
+const reset = () => {
+    for(let i = 0; i < boxes.length; i++){
+        boxes[i].innerText = null
+    }
 }
